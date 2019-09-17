f6f3354aa445cf2c8c8a52836333d742d948fd27
minafaw <gentel_mina2011@yahoo.com>
2019-09-17 23:21:49 +0200

    configure cricleCi to include Percy

--- cats-client/test/specs/basic.js
+++ cats-client/test/specs/basic.js
@@ -1,4 +1,13 @@
 const assert = require("assert");
+const { percySnapshot } = require("@percy/webdriverio");
+
+// In your wdio.conf.js
+before: function () {
+    // Import percySnapshot function
+    const { percySnapshot } = require('@percy/webdriverio');
+    // Make percySnapshot available as a global variable in all wdio tests
+    global.percySnapshot = percySnapshot;
+  }
 
 describe("webdriver.io page", () => {
   it("should have the right title", () => {
@@ -8,5 +17,6 @@ describe("webdriver.io page", () => {
       title,
       "WebdriverIO · Next-gen WebDriver test framework for Node.js"
     );
+    percySnapshot(browser, this.test.fullTitle())
   });
 });
