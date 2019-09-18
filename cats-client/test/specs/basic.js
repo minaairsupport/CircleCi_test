9a5966f52ba903aef867f0d1732c2c4dc2ce865c
minafaw <gentel_mina2011@yahoo.com>
2019-09-18 03:18:31 +0200

    change command

--- cats-client/test/specs/basic.js
+++ cats-client/test/specs/basic.js
@@ -19,7 +19,7 @@ describe("webdriver.io page", () => {
 
   it("should have the right title", () => {
     browser.url("https://webdriver.io");
-    percySnapshot(browser, "webdriver.io page");
+    browser.percySnapshot("webdriver.io page");
     const title = browser.getTitle();
 
     assert.strictEqual(
