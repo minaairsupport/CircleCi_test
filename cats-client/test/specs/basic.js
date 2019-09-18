eac385ec4e6cd755dff73a49e4f1704f724fd9b0
minafaw <gentel_mina2011@yahoo.com>
2019-09-18 03:05:54 +0200

    fix test

--- cats-client/test/specs/basic.js
+++ cats-client/test/specs/basic.js
@@ -2,20 +2,20 @@ const assert = require("assert");
 const httpServer = require("http-server");
 const { percySnapshot } = require("@percy/webdriverio");
 
-let server = null;
-
-before(function() {
-  // Start local server to host app under test.
-  server = httpServer.createServer({ root: `${__dirname}/../site` });
-  server.listen(PORT);
-});
-
-after(function() {
-  // Shut down the HTTP server.
-  server.close();
-});
-
 describe("webdriver.io page", () => {
+  let server = null;
+
+  before(function() {
+    // Start local server to host app under test.
+    server = httpServer.createServer({ root: `${__dirname}/../site` });
+    server.listen(PORT);
+  });
+
+  after(function() {
+    // Shut down the HTTP server.
+    server.close();
+  });
+
   it("should have the right title", () => {
     browser.url("https://webdriver.io");
     percySnapshot(browser, "webdriver.io page");
