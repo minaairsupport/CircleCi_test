62bbafa1b4cf4dd0154060c82b3565744381d635
minafaw <gentel_mina2011@yahoo.com>
2019-09-18 03:55:57 +0200

    one more

--- cats-client/test/specs/basic.js
+++ cats-client/test/specs/basic.js
@@ -20,11 +20,11 @@ describe("webdriver.io page", () => {
   it("should have the right title", () => {
     browser.url("https://webdriver.io");
     percySnapshot(browser, "sample");
-    const title = browser.getTitle().then(function(title) {
-      assert.strictEqual(
-        title,
-        "WebdriverIO · Next-gen WebDriver test framework for Node.js"
-      );
-    });
+    const title = browser.getTitle();
+
+    assert.strictEqual(
+      title,
+      "WebdriverIO · Next-gen WebDriver test framework for Node.js"
+    );
   });
 });
