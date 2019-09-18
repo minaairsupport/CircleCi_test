ed82213a0ba3fe81ab6e562d9cdb9bebf3a84830
minafaw <gentel_mina2011@yahoo.com>
2019-09-18 03:01:59 +0200

    add server

--- cats-client/test/specs/basic.js
+++ cats-client/test/specs/basic.js
@@ -1,13 +1,26 @@
 const assert = require("assert");
+const httpServer = require("http-server");
 const { percySnapshot } = require("@percy/webdriverio");
 
-// In your wdio.conf.js
+let server = null;
+
+before(function() {
+  // Start local server to host app under test.
+  server = httpServer.createServer({ root: `${__dirname}/../site` });
+  server.listen(PORT);
+});
+
+after(function() {
+  // Shut down the HTTP server.
+  server.close();
+});
 
 describe("webdriver.io page", () => {
   it("should have the right title", () => {
     browser.url("https://webdriver.io");
-    const title = browser.getTitle();
     percySnapshot(browser, "webdriver.io page");
+    const title = browser.getTitle();
+
     assert.strictEqual(
       title,
       "WebdriverIO · Next-gen WebDriver test framework for Node.js"
