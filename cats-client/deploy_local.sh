d16a880ca627382e21f06b863a67bc61ddf47799
Mina <33718103+minaairsupport@users.noreply.github.com>
2020-06-10 03:15:11 +0200

    Create deploy_local.sh

---
+++ cats-client/deploy_local.sh
@@ -0,0 +1,15 @@
+if [ $# -ne 1 ]; then
+  echo "Must specify either dev or staging."
+  exit 0;
+fi;
+
+if [ "$1" == "dev" ]; then
+  cp src/api.config.dev.js src/api.config.js
+  npm start
+elif [ "$1" == "staging" ]; then
+  cp src/api.config.staging.js src/api.config.js
+  npm start
+else
+  echo "Must specify either dev or staging."
+  exit 0;
+fi;
